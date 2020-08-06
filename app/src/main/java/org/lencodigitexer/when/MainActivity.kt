8f1904cb6e6cbd9fb215e8564f83c1b88d839dbe
Илья <addminuser258@gmail.com>
2020-08-06 19:18:52 +0300

    проверку на идентичность событий. чтобы на "когда взойдет солнце" не генерировалась новая дата, а сказала бы, что это уже было)

--- app/src/main/java/org/lencodigitexer/when/MainActivity.kt
+++ app/src/main/java/org/lencodigitexer/when/MainActivity.kt
@@ -3,6 +3,7 @@ package org.lencodigitexer.`when`
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import java.text.SimpleDateFormat
 import java.util.*
@@ -17,17 +18,49 @@ class MainActivity : AppCompatActivity() {
 
         val button_date = findViewById<Button>(R.id.button) as Button
         val text_of_date = findViewById<TextView>(R.id.dateText) as TextView
+        var text_of_input_old = "".toString()
+        // text_of_input_old = ""
+
         button_date.setOnClickListener{
+            // ищем поле
+            var text_of_input = findViewById<EditText>(R.id.eventText)
+
+            //берем актуальный текст из поля
+            var text_of_input_text = text_of_input.text.toString()
+
+            if (text_of_input_text.toString() == "".toString()){
+                text_of_date.text = "Поле пустое"
+            }
+
+            //если поле не пустое
+            else if (text_of_input_text.toString() != "".toString()){
+
+                //если тексты разные
+                if (text_of_input_text.toString() != text_of_input_old.toString()){
+                    // выводим статус текущего текста
+                    println(text_of_input_text)
+                    println(text_of_input_old)
+
+                    // и сохраняем этот текст в text_of_input_old
+                    text_of_input_old = text_of_input_text
+
+                    //вывод даты
+                    var rnds = (1000000000..2000000000).random().toLong()
+                    val date = java.util.Date(rnds * 1000)
+                    text_of_date.text = sdf.format(date)
+                }
+            }
+
+
+
+            /*
+            println(text_of_input_text)
+            println(text_of_input_old)
 
             var rnds = (1000000000..2000000000).random().toLong()
-            println(rnds)
-
-
             val date = java.util.Date(rnds * 1000)
+            text_of_date.text = sdf.format(date) */
 
-
-
-            text_of_date.text = sdf.format(date)
         }
     }
 }
